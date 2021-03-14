1b93b506d333462b038ea84a9cd694d7bcb21093
JurgenYu <tigeryhw@gmail.com>
2021-03-15 07:00:32 +0800

    remove firebase api
    Api information can be find in src\Firebase\Firebase.tsx

--- src/Firebase/Firebase.tsx
+++ src/Firebase/Firebase.tsx
@@ -3,13 +3,6 @@ import React, { useEffect, useState, ReactNode } from 'react'
 import FirebaseContext, { inputFormInterface } from './Context';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBmo5wgK-IByqI6qoSmsCMxVSSNFspJd-8",
-    authDomain: "whos-crowded-4ca8f.firebaseapp.com",
-    databaseURL: "https://whos-crowded-4ca8f.firebaseio.com",
-    projectId: "whos-crowded-4ca8f",
-    storageBucket: "whos-crowded-4ca8f.appspot.com",
-    messagingSenderId: "748794973025",
-    appId: "1:748794973025:web:1ae26acc25827cefb25320"
 };
 
 interface FirebaseProviderProps {
